a1d2fbcdf79162cc75bc16ddd6de93184c3e1bb7
Sakari Tanskanen <sakari@noice.com>
2023-11-02 10:25:19 +0200

    build release versions

--- build_docker.sh
+++ build_docker.sh
@@ -2,4 +2,4 @@ cd docker && docker build -t livekit-libwebrtc-builder .
 
 cd ..
 
-docker run --rm -it -e HOME=/tmp -v $(pwd):/workspace livekit-libwebrtc-builder /bin/bash -c "cd /workspace && rustup default stable && LK_CUSTOM_WEBRTC=/workspace/webrtc-sys/libwebrtc/linux-x64-release/ cargo build"
+docker run --rm -it -e HOME=/tmp -v $(pwd):/workspace livekit-libwebrtc-builder /bin/bash -c "cd /workspace && rustup default stable && LK_CUSTOM_WEBRTC=/workspace/webrtc-sys/libwebrtc/linux-x64-release/ cargo build --release"
